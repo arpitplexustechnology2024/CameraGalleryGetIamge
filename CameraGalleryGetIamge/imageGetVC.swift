911515bd91baf9f8b3ea1dc68b12a8c198f39043
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-27 11:16:03 -0700

    Commit

---
+++ CameraGalleryGetIamge/imageGetVC.swift
@@ -0,0 +1,29 @@
+//
+//  imageGetVC.swift
+//  CameraGalleryGetIamge
+//
+//  Created by Arpit iOS Dev. on 27/05/24.
+//
+
+import UIKit
+
+class imageGetVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
