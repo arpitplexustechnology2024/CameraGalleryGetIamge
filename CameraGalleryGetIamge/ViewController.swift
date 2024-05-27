a868d64b285c7c2059da5a6f0b79b7ebacd1e7ea
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-27 10:05:04 -0700

    Initial Commit

---
+++ CameraGalleryGetIamge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraGalleryGetIamge
+//
+//  Created by Arpit iOS Dev. on 27/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
